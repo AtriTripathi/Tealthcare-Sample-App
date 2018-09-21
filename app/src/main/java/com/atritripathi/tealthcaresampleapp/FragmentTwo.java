035d7ff600b8319b0b89d01d3b35beb163f40dfc
Atri Tripathi <tripathi.atri@gmail.com>
2018-09-21 16:06:05 +0530

    Complete task for Fragment Two

--- app/src/main/java/com/atritripathi/tealthcaresampleapp/FragmentTwo.java
+++ app/src/main/java/com/atritripathi/tealthcaresampleapp/FragmentTwo.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -27,10 +28,10 @@ public class FragmentTwo extends Fragment {
 
     private static final String TAG = "FragmentTwo";
 
-    private String MY_URL_STRING;
+    private String IMAGE_URL;
     private static ProgressBar circularProgressBar;
     private static ImageView imageView;
-    private Button loadImage;
+    private static TextView errorMessage;
 
     public FragmentTwo() {
         // Required empty public constructor
@@ -41,13 +42,28 @@ public class FragmentTwo extends Fragment {
         return inflater.inflate(R.layout.fragment_two, container, false);
     }
 
+    public boolean isConnected() {
+        boolean value = false;
+        try {
+            String command = "ping -c 1 google.com";
+            value = Runtime.getRuntime().exec(command).waitFor() == 0;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        Button loadImage = view.findViewById(R.id.button_refresh);
         imageView = view.findViewById(R.id.iv_load_image);
-        loadImage = view.findViewById(R.id.button_refresh);
+        errorMessage = view.findViewById(R.id.tv_error_msg);
         circularProgressBar = view.findViewById(R.id.progressBar_circle);
+
+        errorMessage.setText("Image Placeholder");
 
         final ArrayList<String> imageUrlList = new ArrayList<>();
         imageUrlList.add("https://wallpapercave.com/wp/6K44j5E.jpg");
@@ -59,48 +75,86 @@ public class FragmentTwo extends Fragment {
         loadImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                circularProgressBar.setVisibility(View.VISIBLE);
-                Collections.shuffle(imageUrlList);
-                MY_URL_STRING = imageUrlList.get(0);
-                new DownloadImageTask().execute(MY_URL_STRING);
-                Toast.makeText(getContext(),"Downloading Image", Toast.LENGTH_SHORT).show();
+                if (isConnected()) {
+                    circularProgressBar.setVisibility(View.VISIBLE);
+
+                    // To pick random link
+                    Collections.shuffle(imageUrlList);
+                    IMAGE_URL = imageUrlList.get(0);
+
+                    new DownloadImageTask().execute(IMAGE_URL);
+                    Toast.makeText(getContext(),"Downloading New Image", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(getContext(),"No internet connection. Please check and try again.",Toast.LENGTH_LONG).show();
+                }
             }
         });
-
     }
+
 
     private static class DownloadImageTask extends AsyncTask<String, Integer, Bitmap> {
 
-//        private DownloadImageTask(ImageView imageView) {
-//            this.imageView = imageView;
-//        }
-
         @Override
         protected Bitmap doInBackground(String... Url) {
-            Bitmap image = null;
+            Bitmap resizedImage = null;
             HttpURLConnection connection = null;
             try {
                 URL url = new URL(Url[0]);
                 connection = (HttpURLConnection) url.openConnection();
                 connection.connect();
 
-                InputStream input = new BufferedInputStream(url.openStream(),8192);
-                input.reset();
-                image = BitmapFactory.decodeStream(input);
-                input.close();
+                InputStream inputStream = new BufferedInputStream(url.openStream(), 8192);
+                Log.d(TAG, "doInBackground: inputStream stream = " + inputStream);
+
+                resizedImage = getResizedImage(inputStream,2);
+                Log.d(TAG, "doInBackground: inputStream here = " + resizedImage);
+
+                inputStream.close();
             } catch (Exception e) {
                 Log.e("Error", e.getMessage());
                 e.printStackTrace();
             } finally {
-                connection.disconnect();
+                //connection.disconnect();
                 Log.d(TAG, "doInBackground: connection is closed");
             }
-            return image;
+            connection.disconnect();
+            Log.d(TAG, "doInBackground: input there = " + resizedImage);
+            return resizedImage;
         }
 
+        @Override
         protected void onPostExecute(Bitmap result) {
+            errorMessage.setText("Error Downloading. Try again!");
+            if (result == null) {
+                errorMessage.setVisibility(View.VISIBLE);
+            } else {
+                errorMessage.setVisibility(View.INVISIBLE);
+            }
+
             imageView.setImageBitmap(result);
-            circularProgressBar.setVisibility(View.GONE);
+            circularProgressBar.setVisibility(View.INVISIBLE);
         }
     }
+
+
+    private static Bitmap getResizedImage(InputStream inputStream, int scalingFactor) {
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+
+        options.inSampleSize = scalingFactor;
+        
+        Log.d(TAG, "getResizedImage: inSampleSize = " + options.inSampleSize);
+
+        options.inJustDecodeBounds = false;
+        
+        Bitmap bitmap = BitmapFactory.decodeStream(inputStream,null,options);
+
+        Log.d(TAG, "getResizedImage: Initial bitmap size = " + bitmap.getByteCount());
+        Log.d(TAG, "getResizedImage: Options outwidth = " + options.outWidth);
+        Log.d(TAG, "getResizedImage: Options outheight = " + options.outHeight);
+
+        return bitmap;
+    }
+
 }
