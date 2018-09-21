469fa637b854728771110173467cd35f68de933e
Atri Tripathi <tripathi.atri@gmail.com>
2018-09-22 04:20:03 +0530

    Some retouches to the UI

--- app/src/main/java/com/atritripathi/tealthcaresampleapp/FragmentThree.java
+++ app/src/main/java/com/atritripathi/tealthcaresampleapp/FragmentThree.java
@@ -2,10 +2,10 @@ package com.atritripathi.tealthcaresampleapp;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -24,6 +24,7 @@ public class FragmentThree extends Fragment {
     private static MessageViewModel mMessageViewModel;
     private static Button addDummyChatButton;
     private static TextView statusTextView;
+    private static ConstraintLayout messageCard;
 
     public FragmentThree() {
         // Required empty public constructor
@@ -45,8 +46,9 @@ public class FragmentThree extends Fragment {
 
         // This is used to easily get access to all the views of "message_layout".
         LayoutInflater messageLayout = getLayoutInflater();
-        View messageView = messageLayout.inflate(R.layout.message_layout,null);
+        View messageView = messageLayout.inflate(R.layout.message_layout, null);
         statusTextView = messageView.findViewById(R.id.tv_status);
+        messageCard = messageView.findViewById(R.id.message_card_content);
 
 
         final MessageAdapter mMessageAdapter = new MessageAdapter(getContext());
@@ -80,61 +82,28 @@ public class FragmentThree extends Fragment {
          * and a boolean value to know whether the data was sent or recieved.
          */
         messagesList = new ArrayList<>();
-        messagesList.add(new Chats("Hey Atri!", null, "8:30 AM",false));
-        messagesList.add(new Chats("Hey Doc", "Read", "9:40 AM",true));
-        messagesList.add(new Chats("What's up?!", "Read", "9:55 AM",true));
-        messagesList.add(new Chats("Yeah, just wanted to check on you.", null, "12:30 PM",false));
-        messagesList.add(new Chats("How is your health?", null, "1:25 PM",false));
-        messagesList.add(new Chats("It's better now. The cold is gone.", "Read", "2:00 PM",true));
-        messagesList.add(new Chats("But, I still have some headache.", "Delivered", "3:30 PM",true));
-        messagesList.add(new Chats("Okay then, let's change your medication.", null, "4:00 PM",false));
-        messagesList.add(new Chats("And most importantly, take rest. It is the best healer, ever known.", null, "4:30 PM",false));
-        messagesList.add(new Chats("Sure Doc. And thank you for your precious time. I'll kee you posted.", "Sent", "7:30 PM",true));
-        messagesList.add(new Chats("Your welcome, Atri. Always pleased to serve you.", null, "8:15 PM",false));
+        messagesList.add(new Chats("Hey Atri!", null, "8:30 AM", false));
+        messagesList.add(new Chats("Hey Doc", "Read", "9:40 AM", true));
+        messagesList.add(new Chats("What's up?!", "Read", "9:55 AM", true));
+        messagesList.add(new Chats("Yeah, just wanted to check on you.", null, "12:30 PM", false));
+        messagesList.add(new Chats("How is your health?", null, "1:25 PM", false));
+        messagesList.add(new Chats("It's better now. The cold is gone.", "Read", "2:00 PM", true));
+        messagesList.add(new Chats("But, I still have some headache.", "Delivered", "3:30 PM", true));
+        messagesList.add(new Chats("Okay then, let's change your medication.", null, "4:00 PM", false));
+        messagesList.add(new Chats("And most importantly, take rest. It is the best healer, ever known.", null, "4:30 PM", false));
+        messagesList.add(new Chats("Sure Doc. And thank you for your precious time. I'll kee you posted.", "Sent", "7:30 PM", true));
+        messagesList.add(new Chats("Your welcome, Atri. Always pleased to serve you.", null, "8:15 PM", false));
 
 
-        // Handling the DB operations on a background thread, i.e. AsyncTask
-        new PopulateChatTask().execute();
-
-    }
-
-
-    private static class PopulateChatTask extends AsyncTask<Void,Void,Void> {
-        @Override
-        protected Void doInBackground(Void... voids) {
-
-            addDummyChatButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    for (Chats chat: messagesList) {
-
-                        if(chat.getIsSentMessage()) {
-                            statusTextView.setText(chat.getStatus());           // Update the TextView with "Status" for sent messages.
-                        } else {
-                            statusTextView.setText(chat.getTime());             // Update the same TextView with "Time" for the received messages.
-                        }
-
-                        Message message = new Message(chat.getMessageText(), chat.getStatus(), chat.getTime(), chat.getIsSentMessage());
-                        mMessageViewModel.insert(message);
-                    }
+        addDummyChatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (Chats chat : messagesList) {
+                    Message message = new Message(chat.getMessageText(), chat.getStatus(), chat.getTime(), chat.getIsSentMessage());
+                    mMessageViewModel.insert(message);
                 }
-            });
-            return null;
-        }
-
-
-        /**
-         * The below function can be used to update the activity with the "Status" values, as
-         * the onPostExecute runs on the UI thread. But, I was unable to get it running, for now.
-         * As I think the issue might be because we are trying to access the UI thread from
-         * a Fragment, which itself is being handled separately. We can look into this issue.
-         */
-//        @Override
-//        protected void onPostExecute(Void aVoid) {
-//            super.onPostExecute(aVoid);
-//            statusTextView.setText("Done");
-//        }
-
-
+            }
+        });
     }
 }
+
--- app/src/main/java/com/atritripathi/tealthcaresampleapp/MessageAdapter.java
+++ app/src/main/java/com/atritripathi/tealthcaresampleapp/MessageAdapter.java
@@ -2,10 +2,13 @@ package com.atritripathi.tealthcaresampleapp;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,6 +16,8 @@ import java.util.List;
 public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
     private final LayoutInflater mInflater;
     private List<Message> mMessages;
+    private static CardView messageCardHolder;
+    private static RelativeLayout.LayoutParams layoutParams;
 
 
     public MessageAdapter(Context context) {
@@ -22,10 +27,19 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
 
     public static class MessageViewHolder extends RecyclerView.ViewHolder {
         private final TextView messageItemView;
+        private final TextView statusTextView;
+        private final ConstraintLayout messageCardContent;
+
+
 
         private MessageViewHolder(final View itemView) {
             super(itemView);
             messageItemView = itemView.findViewById(R.id.tv_message);
+            statusTextView = itemView.findViewById(R.id.tv_status);
+            messageCardContent = itemView.findViewById(R.id.message_card_content);
+            messageCardHolder = itemView.findViewById(R.id.message_card_holder);
+            layoutParams = (RelativeLayout.LayoutParams) messageCardHolder.getLayoutParams();
+
         }
     }
 
@@ -41,7 +55,21 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageV
     @Override
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
         if (mMessages != null) {
+
             holder.messageItemView.setText(mMessages.get(position).getMessageText());
+
+            if (mMessages.get(position).getIsSentMessage()) {
+                holder.statusTextView.setText(mMessages.get(position).getStatus());
+                holder.messageCardContent.setBackgroundResource(R.color.light_yellow);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+                messageCardHolder.setLayoutParams(layoutParams);
+            } else {
+                holder.statusTextView.setText(mMessages.get(position).getTime());
+                holder.messageCardContent.setBackgroundResource(R.color.sky_blue);
+                layoutParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+                messageCardHolder.setLayoutParams(layoutParams);
+            }
+
         } else {
             holder.messageItemView.setText("No Messages");
         }
